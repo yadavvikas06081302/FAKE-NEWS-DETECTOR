c0e3e618bbb2bf2a9c87dd84ea66dcfbf80abcf4
VIKAS YADAV <yadavvikas06081302@gmail.com>
2026-09-19 20:37:24 +0530

    app.py
    Added NLTK download for stopwords and cleaned up imports.

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import numpy as np
 import re
 import pandas as pd
+import nltk
+
+nltk.download('stopwords')
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
